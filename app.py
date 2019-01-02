f8988df01d6c701b4f31f6dae5a16a7453e651e5
redpanda77 <gonzalocasajus@gmail.com>
2019-01-02 17:46:51 +0100

    user functionality

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
-app = Flask(__name__)
-
 from db import db
 from resources.restaurant import RestaurantList
+from resources.user import UserRegister, UserLogin, User, TokenRefresh, UserLogout
+
+app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -28,6 +29,12 @@ jwt = JWTManager(app)
 #api.add_resource(Restaurant, "/restaurant/<string:name>")
 #api.add_resource(RestaurantSearch, "/restaurant/search")
 api.add_resource(RestaurantList, "/restaurants/<string:city>/<string:query>")
+
+api.add_resource(UserRegister, "/register")
+api.add_resource(User, "/user/<int:user_id>")
+api.add_resource(UserLogin, "/login")
+api.add_resource(TokenRefresh, "/refresh")
+api.add_resource(UserLogout, "/logout")
 
 if __name__ == "__main__":
     db.init_app(app)
